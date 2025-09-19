b3b5946b361683c6dfe484fd1e816c3a2568a219
Mikael Toresen <mikael.toresen@gmail.com>
2025-09-19 13:43:04 +0200

    fix setup

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 pip install uv
 #curl -LsSf https://astral.sh/uv/install.sh | sh
 uv sync
-cmd="/home/miktoki/waves-on-map/.venv/bin/uvicorn --app-dir /home/miktoki/waves-on-map --uds \"${DOMAIN_SOCKET}\" app_map:app"
-uv run pa website create --domain miktoki.pythonanywhere.com --command "$cmd"
+# shellcheck disable=SC2016
+uv run pa website create --domain miktoki.pythonanywhere.com --command '/home/miktoki/waves-on-map/.venv/bin/uvicorn --app-dir /home/miktoki/waves-on-map --uds ${DOMAIN_SOCKET} app_map:app'
 # pa website create-autorenew-cert --domain YOURCUSTOMDOMAIN
 # pa website reload --domain YOURUSERNAME.pythonanywhere.com
 
